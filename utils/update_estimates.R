514f5529fedffc2574c2bc450703bc781798e391
biocyberman <biocyberman@gmail.com>
2021-09-16 11:47:44 +0200

    Updated website

--- utils/update_estimates.R
+++ utils/update_estimates.R
@@ -1,6 +1,6 @@
 
 # get function for updating Rt estimates
 # source("https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/master/R/get_estimates.R")
-library("covidrtestimates")
-# source("~/repositories/covid-rt-estimates/R/get_estimates.R")
-get_estimates("../covid-rt-estimates", include = c("csv","rds", "png"))
+# library("covidrtestimates")
+source("~/repositories/covid-rt-estimates/R/get_estimates.R")
+get_estimates("../covid-rt-estimates", include = c("csv","rds", "png"), patterns=c("*global*", "*[Vv]ietnam*"))
